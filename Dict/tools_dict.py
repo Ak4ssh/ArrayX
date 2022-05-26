7801b21ecdc8363a1dc52f1c3eb3512eaed2e166
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-26 11:53:34 +0530

    Update tools_dict.py

--- Dict/tools_dict.py
+++ Dict/tools_dict.py
@@ -4,7 +4,7 @@
 TOOL_DICT = dict()
 
 TOOL_DICT["Vc Raid"] = f"""
-__You Can Raid Voice Call Of Groups By Using__ `/vcraid <chat_username>` __Command__.
+__You Can Raid Voice Call Of Groups By Using__ /vcraid `<chat_username>` __Command__.
 
 **Note: First You Have To Join All Your Ids With Command** `/join <chat_username>` **To Raid Group Calls**.
 """
